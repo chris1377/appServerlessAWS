74a79259bd234dd99993e82d1ac8a908a9589b72
Christian Candela <83507012+chris1377@users.noreply.github.com>
2026-07-31 15:57:28 +0200

    Add files via upload

---
+++ listaUtenti/Risultato.java
@@ -0,0 +1,37 @@
+package dev.chris;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Risultato {
+
+	private String risult;
+	private List<String> utenti;
+	
+	public Risultato() {
+		utenti = new ArrayList<String>();
+	}
+	
+	public String getRisult() {
+		return risult;
+	}
+	public void setRisult(String risult) {
+		this.risult = risult;
+	}
+	public List<String> getUtenti() {
+		return utenti;
+	}
+	public void setUtenti(List<String> utenti) {
+		this.utenti = utenti;
+	}
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+}
