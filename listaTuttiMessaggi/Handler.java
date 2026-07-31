88f317ee44c380ff11eb73ba4f4b09e56e5e1afd
Christian Candela <83507012+chris1377@users.noreply.github.com>
2026-07-31 15:54:39 +0200

    Add files via upload

---
+++ listaTuttiMessaggi/Handler.java
@@ -0,0 +1,63 @@
+package dev.chris;
+
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+
+
+public class Handler implements RequestHandler<Void, Risultato> {
+	
+    @Override
+    public Risultato handleRequest(Void map, Context context) {
+        // Configura il client di Amazon Cognito
+    	DynamoDbClient dynamoDb = DynamoDbClient.builder()
+                .region(Region.EU_NORTH_1) 
+                .build();
+
+        // Specifica i parametri per la query
+        ScanRequest queryRequest = ScanRequest.builder()
+                .tableName("messaggi") // Nome della tabella "ordini"
+                .build();
+
+        // Esegui la query
+        ScanResponse response = dynamoDb.scan(queryRequest);
+        dynamoDb.close();
+        // Elabora i risultati
+        Risultato ris = new Risultato();
+        ris.setRisult("Fail");
+        List<List<String>> totale = new ArrayList<List<String>>();
+        for (Map<String, AttributeValue> item : response.items()) {
+        	List<String> tupla = new ArrayList<String>();
+        	ris.setRisult("Ok");
+        	tupla.add(item.get("destinatario").s());
+        	tupla.add(item.get("id").s());
+        	tupla.add(item.get("letto").s());
+        	tupla.add(item.get("messaggio").s());
+        	tupla.add(item.get("mittente").s());
+        	totale.add(tupla);
+            
+        }
+        ris.setMessaggi(totale);
+        return ris;
+       
+    }
+
+	
+  }
